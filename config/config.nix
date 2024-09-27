47bb9e7ed0ea3c55d2b6a0f89e73875ead520233
kix300 <kixwalkiki@gmail.com>
2024-09-27 22:57:21 +0200

    First Commit

---
+++ config/config.nix
@@ -0,0 +1,16 @@
+{self, ...}:
+{
+	config.opts = {
+		tabstop = 4;
+		shiftwidth = 4;
+		expandtab = false;
+		number = true;
+		relativenumber = true;
+		confirm = true;
+		smartindent = true;
+		wildmode = "longest:full,full";
+		undofile = true;
+		undolevels = 10000;
+		completeopt = "menu,menuone,noselect";
+	};
+}
